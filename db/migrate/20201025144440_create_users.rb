3b421b64bcf11fda6b404a153e5b3a92c2699bab
morelife09 <junichi1995930@gmail.com>
2020-10-26 00:14:42 +0900

    commit

--- db/migrate/20201025144440_create_users.rb
+++ db/migrate/20201025144440_create_users.rb
@@ -1,14 +1,22 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :id
-      t.string :nickname
-      t.string :email
-      t.string :password
-      t.string :first_name
-      t.string :family_name
-      t.date :birth_date
-      t.timestamps
+      t.string :nickname, null: false
+      t.string :first_name, null: false
+      t.string :family_name, null: false
+      t.date :birth_date, null: false
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
     end
   end
 end
