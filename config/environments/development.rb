f577130b7baa583ed2913f4b3811ff15bc48e77a
morelife09 <junichi1995930@gmail.com>
2020-11-07 19:30:26 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.sass.inline_source_maps = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
